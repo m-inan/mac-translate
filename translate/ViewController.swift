de30b04aba2547afbd2b8857c378eba09c0f643b
m-inan <muhammet@tilpark.com>
2023-01-15 17:04:46 +0300

    Initial Commit

---
+++ translate/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  translate
+//
+//  Created by Minan on 15.01.2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
