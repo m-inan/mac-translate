ba14371000013bcc580109f150921eb1724f05f3
m-inan <muhammet@tilpark.com>
2023-01-17 05:45:35 +0300

    Add css for remove unnecessary ui element

--- translate/AppDelegate.swift
+++ translate/AppDelegate.swift
@@ -12,7 +12,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     public var window: NSWindow!
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        window = NSWindow(contentRect: NSRect(x: 0, y: 0, width: 400, height: 400), styleMask: [.titled, .closable, .resizable], backing: .buffered, defer: false)
+        window = NSWindow(contentRect: .zero, styleMask: [.titled, .closable, .resizable], backing: .buffered, defer: false)
         window.center()
         window.title = "Translate"
         window.contentViewController = ViewController()
--- translate/ViewController.swift
+++ translate/ViewController.swift
@@ -8,15 +8,39 @@
 import Cocoa
 import WebKit
 
-class ViewController: NSViewController {
+
+class ViewController: NSViewController, WKNavigationDelegate {
     var webView: WKWebView!
     
     override func loadView() {
-        webView = WKWebView(frame: NSRect(x: 0, y: 0, width: 400, height: 400))
+        webView = WKWebView(frame: NSRect(x: 0, y: 0, width: 400, height: 334))
+        webView.navigationDelegate = self
         
-        let url = URL(string: "https://translate.google.com/?sl=en&tl=tr&op=translate")!
+        let url = URL(string: "https://translate.google.com/?sl=en&tl=tr")!
         webView.load(URLRequest(url: url))
         
         self.view = webView
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        // disable google translate autocomplete and remove ui elements
+        self.webView.evaluateJavaScript("""
+            const style = document.createElement("style");
+        
+            style.innerHTML = `
+                header,
+                #kvLWu,
+                .VjFXz,
+                .VlPnLc,
+                .gp-footer,
+                .hgbeOc.EjH7wc,
+                .feedback-link,
+                [aria-haspopup] {
+                    display: none !important
+                }
+            `;
+        
+            document.body.appendChild(style);
+        """)
     }
 }
